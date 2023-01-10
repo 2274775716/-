8ee3c85ffd40e56fdb5d43c294823a5a6ee8a358
2274775716 <97444642+2274775716@users.noreply.github.com>
2023-01-10 15:32:47 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hello World !"
+echo "于学智，2023 1 10 15：21"
+shell_name="bash"
+echo $shell_name
+echo `date`
+echo `ps -A `
+echo `ps -u root`
+echo `procinfo -a`
+echo `procinfo -s`
+echo `top`
+exit
